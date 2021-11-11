1004a530427ee0edc83fffb4a586edaa04a8717b
Victoria045 <victoriaberyl45@gmail.com>
2021-11-11 08:10:03 +0300

    create the application instance

---
+++ manage.py
@@ -0,0 +1,25 @@
+from app import create_app
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+
+manager.add_command('server', Server)
+
+
+@manager.command 
+def test()
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().dicsover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app)
+
+if __name__ == 'main':
+    manager.run()
