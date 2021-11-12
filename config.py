3ebc4a59faee573ddb10527e25899463692a504b
Victoria045 <victoriaberyl45@gmail.com>
2021-11-12 06:46:47 +0300

    pass in the SQLAlchemy URI in the config file

--- config.py
+++ config.py
@@ -13,6 +13,14 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 class DevConfig(Config):
     '''
     Development  configuration child class
