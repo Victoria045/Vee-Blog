a36b8ea0b8886ab8478ac46c469eaa50d645a8fe
Victoria045 <victoriaberyl45@gmail.com>
2021-11-12 06:59:37 +0300

    import, initialize the flask extemsions and register the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap 
-from config import config_options 
+from config import config_options
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy 
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import mail  
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 
 def create_app(config_name):
+
     app = Flask(__name__)
 
     # Creating the app configurations
@@ -13,9 +27,16 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app) 
+    login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     
     return app
