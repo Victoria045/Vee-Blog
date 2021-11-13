47bd96bda23c26ec96ffa30165807e264ee986ee
Victoria045 <victoriaberyl45@gmail.com>
2021-11-13 08:41:31 +0300

    import the database from the root

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, PhotoProfile
@@ -23,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app)
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
