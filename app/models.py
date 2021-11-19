6cc6ecaf99b97f5ae34635311fb73959c9af6a70
Victoria045 <victoriaberyl45@gmail.com>
2021-11-19 14:15:02 +0300

    modify the model.py file

--- app/models.py
+++ app/models.py
@@ -28,8 +28,6 @@ class User(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
-
-
     photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
 
     @property
